2dea8e0e2c984d47622ea18576f26a193c35477e
Peter Costa <petersvcosta@gmail.com>
2024-04-26 19:48:17 -0300

    Add files via upload

---
+++ src/main/java/com/example/desafioRpe/DesafioRpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioRpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioRpe/DesafioRpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioRpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
